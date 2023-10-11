ef8c74d5dd585668111fda033908f5faa936c762
Soydan Yuece <soydan.yuece@gmx.de>
2023-10-11 18:13:23 +0200

    Delete console logs in verify token

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -4,13 +4,11 @@ const verifyToken = (req, res, next) => {
   const { token } = req.body;
 
   if (token) {
-    console.log(token);
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
       if (err) {
         res.redirect("login");
       } else {
         req.decoded = decoded;
-        console.log(decoded);
         next();
       }
     });
